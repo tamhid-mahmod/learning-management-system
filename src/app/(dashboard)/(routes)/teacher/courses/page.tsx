e9eccfa1574785f01fd62ee9c6ac604eace257db
tamhid-mahmod <tamhidmahmod94@gmail.com>
2023-11-09 22:41:10 +0600

    added course data table

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,27 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
