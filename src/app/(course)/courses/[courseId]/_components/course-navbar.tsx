5e70c18fe4e2a52787646d0237cfdc59b5b7b927
tamhid-mahmod <tamhidmahmod94@gmail.com>
2023-11-11 15:25:24 +0600

    added course and chapter layout

---
+++ src/app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -0,0 +1,23 @@
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+import { NavbarRoutes } from "@/components/navbar-routes";
+import { CourseMobileSidebar } from "./course-mobile-sidebar";
+
+interface CourseSidebarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+export const CourseNavbar = ({ course, progressCount }: CourseSidebarProps) => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
+
+      <NavbarRoutes />
+    </div>
+  );
+};
